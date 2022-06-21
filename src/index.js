efbcd20aa14344a0c1bdb3c3f51add6e6f033db0
FacuF99 <facundo42081757@gmail.com>
2022-06-21 01:26:40 -0300

    revert

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const client = require("./twitterClient.js");
+
+const tweet = async () => {
+  try {
+    const mediaId = await client.v1.uploadMedia('image.jpg');
+     const newTweet = await client.v2.tweet({media: {media_ids: [mediaId]}} );
+     return newTweet
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+function tick(){
+    var hours = new Date().getHours();
+    if (hours == "00" || hours == "12"){
+        tweet()
+    }
+}
+
+setInterval( tick, 3600000 );
